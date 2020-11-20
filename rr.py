26bb7579f03e671bb982ac16827ca2539a9dc9e5
Raul M <rauloctaviomurcia@gmail.com>
2020-11-20 10:30:31 -0700

    calendarizadores importan modulo proceso

--- rr.py
+++ rr.py
@@ -1,38 +1,5 @@
 import sys
-
-#Clase básica de un proceso
-class Proceso:
-    #Se construye a partir de un id y los periodos de CPU que necesita
-    def __init__(self, Id, tiempo_llegada, cpu_burst):
-        self.__id = Id
-        self.__tiempo_llegada = tiempo_llegada
-        self.__cpu_burst = cpu_burst
-        self.__tiempo_espera = 0
-
-    # Este método suma una unidad al tiempo de espera del proceso
-    def actualizar_tiempo_espera(self):
-        self.__tiempo_espera += 1
-    
-    # Este método resta una unidad a los periodos de cpu del proceso
-    def actualizar_cpu_burst(self):
-        self.__cpu_burst -= 1
-
-    # Este método regresa los periodos restantes del proceso
-    def obtener_cpu_burst(self):
-        return self.__cpu_burst
-
-    # Este método regresa el tiempo de espera del proceso 
-    def obtener_tiempo_espera(self):
-        return self.__tiempo_espera
-
-    # Este método regresa el Id del proceso
-    def obtener_id(self):
-        return self.__id
-
-    # Este método regresa el tiempo de llegada del proceso
-    def obtener_tiempo_llegada(self):
-        return self.__tiempo_llegada
-#-----------------------------------------------------------------------------
+from proceso import Proceso
 
 # Esta función obtiene los procesos que llegan en un tiempo determinado
 def obtener_nuevos_procesos(procesos, t):
@@ -70,11 +37,11 @@ cola = obtener_nuevos_procesos(procesos, 0)
 
 while True:
     #Si hay cola, entonces se toma el siguiente proceso
-    if cola:         
+    if cola:
         cpu = cola.pop(0)
-    # Si no hay cola y el cpu está desocupado, pero existen procesos, 
+    # Si no hay cola y el cpu está desocupado, pero existen procesos,
     # entonces los esperamos
-    elif procesos and cpu == None: 
+    elif procesos and cpu == None:
         t += 1
         # Se obtienen los procesos que llegaron en el tiempo actual
         cola.extend(obtener_nuevos_procesos(procesos, t))
@@ -98,11 +65,11 @@ while True:
         cpu.actualizar_cpu_burst()
         for p in cola:
             p.actualizar_tiempo_espera()
-        
+
         # Se obtienen los procesos que llegaron en el tiempo actual
         cola.extend(obtener_nuevos_procesos(procesos, t))
 
-        # Si el cpu burst del proceso actual llegó a cero, entonces ya no 
+        # Si el cpu burst del proceso actual llegó a cero, entonces ya no
         # necesita el cpu y este se libera
         if cpu.obtener_cpu_burst() == 0:
             proceso_termino = True
--- srtf.py
+++ srtf.py
@@ -1,47 +1,8 @@
 import sys
 import heapq
+from proceso import Proceso
 
-#Clase básica de un proceso
-class Proceso:
-    #Se construye a partir de un id y los periodos de CPU que necesita
-    def __init__(self, id, periodos, tiempo_llegada):
-        self.__id = id
-        self.__periodos = periodos
-        self.__tiempo_espera = 0
-        self.__tiempo_llegada = tiempo_llegada
-
-    # Este método suma una unidad al tiempo de espera del proceso
-    def actualizar_tiempo_espera(self):
-        self.__tiempo_espera += 1
-
-    # Este método resta una unidad a los periodos de cpu del proceso
-    def actualizar_periodos(self):
-        self.__periodos -= 1
-
-    # Este método regresa el tiempo de llegada del proceso
-    def obtener_tiempo_llegada(self):
-        return self.__tiempo_llegada
-
-    # Este método regresa los periodos restantes del proceso
-    def obtener_periodos_restantes(self):
-        return self.__periodos
-
-    # Este método regresa el tiempo de espera del proceso
-    def obtener_tiempo_espera(self):
-        return self.__tiempo_espera
-
-    # Este método regresa el Id del proceso
-    def obtener_id(self):
-        return self.__id
-
-    # La Comparación de procesos se realiza por su tiempo restante
-    def __lt__(self, otro):
-        return self.__periodos < otro.obtener_periodos_restantes()
-
-    def __le__(self, otro):
-        return self.__periodos <= otro.obtener_periodos_restantes()
-
- #Se lee la info de los procesos del archivo de texto
+# Se lee la info de los procesos del archivo de texto
 procesos = [p.strip().split(',') for p in open(sys.argv[1],'r').readlines()]
 
 # Se construye cada proceso y se ordenan respecto a tiempo de llegada
@@ -67,8 +28,8 @@ while procesos or cola_listos:
         # El proceso actual siempre será el que tenga menor tiempo restante
         proceso_actual = cola_listos[0]
 
-        # actualiza periodos
-        proceso_actual.actualizar_periodos()
+        # Actualiza periodos
+        proceso_actual.actualizar_cpu_burst()
 
         # Procesos que no se están ejecutando
         for p in cola_listos[1:]:
@@ -76,7 +37,7 @@ while procesos or cola_listos:
 
         t += 1
 
-        if proceso_actual.obtener_periodos_restantes() == 0:
+        if proceso_actual.obtener_cpu_burst() == 0:
             print("Proceso:", proceso_actual.obtener_id(), end=" ")
             print("terminó en el tiempo:", t, end=" ")
             print("Con tiempo de espera de:", proceso_actual.obtener_tiempo_espera())
